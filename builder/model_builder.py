fdf13f2b02570be3331b335b4b7567cffd15744b
ashrunis <anqiuyu01@gmail.com>
2026-07-02 05:16:31 +0800

    Initial commit

--- builder/model_builder.py
+++ builder/model_builder.py
@@ -1,8 +1,8 @@
 # -*- coding:utf-8 -*-
 
 from network.cylinder_spconv_3d import get_model_class
-from network.segmentator_3d_asymm_spconv import Asymm_3d_spconv
-# from network.segmentator_3d_asymm_spconv_fr_ugfa import Asymm_3d_spconv
+# from network.segmentator_3d_asymm_spconv import Asymm_3d_spconv
+from network.segmentator_3d_asymm_spconv_fr_ugfa import Asymm_3d_spconv
 from network.cylinder_fea_generator import cylinder_fea
 
 
